1d5c1d6404cd5fa5ebbd820715ce4a919635d487
Lugelio <luismaria898@gmail.com>
2026-02-17 20:25:53 -0300

    portfolio terminado

--- javascript.js
+++ javascript.js
@@ -1,14 +1,15 @@
-// Seleccionamos todos los botones que abren modales
+
 const openButtons = document.querySelectorAll(".info-button button");
-// Seleccionamos todos los botones de cerrar
+
 const closeButtons = document.querySelectorAll(".close-button");
-// Seleccionamos todos los modales
+
 const modals = document.querySelectorAll(".modal");
 
-// Función para abrir el modal correcto
+const detailButton = document.getElementById("btn-detalles")
+
 openButtons.forEach(button => {
     button.addEventListener("click", () => {
-        // Obtenemos el ID del modal desde el contenedor padre (el div .info-button)
+      
         const modalId = button.parentElement.id.replace("info-button_", "modal-").toLowerCase();
         const targetModal = document.getElementById(modalId);
         
@@ -20,17 +21,26 @@ openButtons.forEach(button => {
     });
 });
 
-// Función para cerrar los modales
+
 closeButtons.forEach(button => {
     button.addEventListener("click", () => {
         const modal = button.closest(".modal");
         modal.style.opacity = "0";
         modal.style.pointerEvents = "none";
-        document.body.style.overflow = "auto"; // Devuelve el scroll
+        document.body.style.overflow = "auto"; 
     });
 });
 
-// Cerrar al hacer click fuera de la tarjeta (en el fondo oscuro)
+detailButton.addEventListener("click", (e) => {
+        e.preventDefault(); 
+        const targetModal = document.getElementById("modal-detalles");
+        
+        targetModal.style.opacity = "1";
+        targetModal.style.pointerEvents = "auto";
+        document.body.style.overflow = "hidden"; 
+
+    });
+
 window.addEventListener("click", (e) => {
     modals.forEach(modal => {
         if (e.target === modal) {
